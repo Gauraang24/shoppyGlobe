7426ad7e7b060154bd179028fb517eea339f4061
Gauraang24 <garryappdroid@gmail.com>
2024-11-19 23:36:36 +0530

    Navbar : Added AntD icon package and created Navbar Component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { HeartTwoTone, ShoppingTwoTone } from "@ant-design/icons";
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+
+        <nav className='flex justify-between max-w-[1500px] w-screem mx-auto p-3'>
+            <div className='flex items-center gap-6 cursor-pointer'>
+                <img src="/images/favicon/favicon.png" alt="Logo" />
+                <p className='font-bold text-2xl'>ShoppyGlobe</p>
+            </div>
+
+            <div className='flex items-center gap-5 '>
+
+                <ShoppingTwoTone style={{
+                    fontSize: "24px"
+                }} className='cursor-pointer' />
+
+
+                <HeartTwoTone style={{
+                    fontSize: "24px"
+                }} className='cursor-pointer' />
+
+
+            </div>
+
+        </nav>
     )
 }
 
