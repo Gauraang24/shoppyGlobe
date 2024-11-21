8f2b2614d0e2c278fd3845d48f3c50d3b2f826e7
Gauraang24 <garryappdroid@gmail.com>
2024-11-21 22:12:12 +0530

    Performance Optimisation : Lazy loading of components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import Layout from './Components/Layout'
-import Home from './Pages/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import ProductDetails from './Pages/ProductDetails'
-import Cart from './Pages/Cart'
-import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './Components/Layout';
+import { Spin } from 'antd';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Home = React.lazy(() => import('./Pages/Home'));
+const ProductDetails = React.lazy(() => import('./Pages/ProductDetails'));
+const Cart = React.lazy(() => import('./Pages/Cart'));
+const PageNotFound = React.lazy(() => import('./Components/PageNotFound'));
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/productDetails/:id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-        </Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            <Spin size="large" tip="Loading..." />
+          </div>
+        }
+      >
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/productDetails/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+
+            {/* Catch all undefined routes */}
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
       <ToastContainer />
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
